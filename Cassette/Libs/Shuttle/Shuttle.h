cc0fe931af6b4a2b81c8c437f16d43ae3aa65a75
Stephen Hatton <s.hatton@volcanic.co.uk>
2016-04-21 22:42:11 +0100

    + Started work on new lib 'Shuttle', uses AFNetworking, RXPromise for handling HTTP requests

---
+++ Cassette/Libs/Shuttle/Shuttle.h
@@ -0,0 +1,13 @@
+//
+//  Shuttle.h
+//  Cassette
+//
+//  Created by Stephen Hatton on 21/04/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Shuttle : NSObject
+
+@end
---
+++ Cassette/Libs/Shuttle/Shuttle.m
@@ -0,0 +1,13 @@
+//
+//  Shuttle.m
+//  Cassette
+//
+//  Created by Stephen Hatton on 21/04/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import "Shuttle.h"
+
+@implementation Shuttle
+
+@end
