7ec1fb1735c030daa78c32e6ce4afcadc2e3557a
Stephen Hatton <s.hatton@volcanic.co.uk>
2016-04-24 17:36:16 +0100

    + New model: Play Queue, stores the currently playing mix, and queued mixes

---
+++ Cassette/Models/Play Queue/CSTPlayQueue.h
@@ -0,0 +1,13 @@
+//
+//  CSTPlayQueue.h
+//  Cassette
+//
+//  Created by Stephen Hatton on 24/04/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSTPlayQueue : NSObject
+
+@end
---
+++ Cassette/Models/Play Queue/CSTPlayQueue.m
@@ -0,0 +1,13 @@
+//
+//  CSTPlayQueue.m
+//  Cassette
+//
+//  Created by Stephen Hatton on 24/04/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import "CSTPlayQueue.h"
+
+@implementation CSTPlayQueue
+
+@end
