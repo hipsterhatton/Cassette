0ea904b11967a12cdffef8e1725aefc11127a82a
Stephen Hatton <s.hatton@volcanic.co.uk>
2016-04-27 22:23:33 +0100

    + New controller: Collections, used for getting collections, and CRUD operations

---
+++ Cassette/Controllers/Collections/CSTCollectionsController.h
@@ -0,0 +1,13 @@
+//
+//  CSTCollectionsController.h
+//  Cassette
+//
+//  Created by Stephen Hatton on 27/04/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import "CSTParentController.h"
+
+@interface CSTCollectionsController : CSTParentController
+
+@end
---
+++ Cassette/Controllers/Collections/CSTCollectionsController.m
@@ -0,0 +1,13 @@
+//
+//  CSTCollectionsController.m
+//  Cassette
+//
+//  Created by Stephen Hatton on 27/04/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import "CSTCollectionsController.h"
+
+@implementation CSTCollectionsController
+
+@end
