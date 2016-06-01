613fe2062d8fa4cb24fe087e7f48f215f853643e
Stephen Hatton <s.hatton@volcanic.co.uk>
2016-06-01 22:51:24 +0100

    + New controller "Searching" used for autocomplete search, and smart ID mix search

---
+++ Cassette/Controllers/Searching/CSTSearchingController.h
@@ -0,0 +1,13 @@
+//
+//  CSTSearchingController.h
+//  Cassette
+//
+//  Created by Stephen Hatton on 01/06/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import "CSTParentController.h"
+
+@interface CSTSearchingController : CSTParentController
+
+@end
---
+++ Cassette/Controllers/Searching/CSTSearchingController.m
@@ -0,0 +1,13 @@
+//
+//  CSTSearchingController.m
+//  Cassette
+//
+//  Created by Stephen Hatton on 01/06/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import "CSTSearchingController.h"
+
+@implementation CSTSearchingController
+
+@end
