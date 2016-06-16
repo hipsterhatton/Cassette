e14a125f09a225fb893a321fe8685c387076fa42
Stephen Hatton <s.hatton@volcanic.co.uk>
2016-06-16 18:27:18 +0100

    + New parent model: CST Parent Model, has method for returning object ID converted from possible int to NSString

---
+++ Cassette/Models/Parent Model/CSTParentModel.h
@@ -0,0 +1,13 @@
+//
+//  CSTParentModel.h
+//  Cassette
+//
+//  Created by Stephen Hatton on 16/06/2016.
+//  Copyright © 2016 Stephen Hatton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSTParentModel : NSObject
+
+@end
